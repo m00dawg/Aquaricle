f1456b62b4b51bc71fb5333b1b8941117a8fc27a
Tim Soderstrom <tim@moocowproductions.org>
2012-10-27 15:58:10 -0500

    CSS improvements and added equipment log

--- aquarilog/models.py
+++ aquarilog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.forms import ModelForm
 from aquaridawg.fields import EnumField
+
+# Models
 
 class WaterProfile(models.Model):
     waterProfileID = models.AutoField(primary_key=True)
@@ -35,10 +38,10 @@ class WaterLog(models.Model):
     temperature = models.DecimalField(verbose_name='Temperature (C)',max_digits=4,decimal_places=2,null=True,blank=True)    
     ammonia = models.DecimalField(max_digits=3,decimal_places=2,null=True,blank=True)
     nitrites = models.DecimalField(max_digits=3,decimal_places=2,null=True,blank=True)
-    nitrates = models.DecimalField(max_digits=3,decimal_places=2,null=True,blank=True)
+    nitrates = models.DecimalField(max_digits=5,decimal_places=2,null=True,blank=True)
     pH = models.DecimalField(verbose_name='pH',max_digits=3,decimal_places=1,null=True,blank=True)
     KH = models.PositiveSmallIntegerField(null=True,blank=True)
-    amountExchanged = models.PositiveSmallIntegerField(verbose_name='Amount Exchanged',null=True,blank=True)
+    amountExchanged = models.PositiveSmallIntegerField(verbose_name='Water Exchanged',null=True,blank=True)
     comments = models.TextField(blank=True)
     def __unicode__(self):
         return unicode(self.testedOn)
@@ -51,7 +54,8 @@ class Equipment(models.Model):
     equipmentID = models.AutoField(primary_key=True)
     aquariumID = models.ForeignKey(Aquarium,verbose_name='Aquarium',db_column='aquariumID')
     installDate = models.DateTimeField(verbose_name='Install Date',editable=True,blank=False)
-    name = models.CharField(max_length=24,unique=True)
+    name = models.CharField(max_length=64,unique=True)
+    url = models.URLField(max_length=255,blank=True)
     maintenanceInterval = models.PositiveSmallIntegerField(verbose_name='Maintenance Interval (Days)',null=True,blank=True)
     comments = models.TextField(blank=True)
     def __unicode__(self):
@@ -65,10 +69,17 @@ class EquipmentLog(models.Model):
     equipmentLogID = models.AutoField(primary_key=True)
     equipmentID = models.ForeignKey(Equipment,verbose_name='Equipment',db_column='equipmentID')
     logDate = models.DateTimeField(verbose_name='Date',editable=True,blank=False)
-    maintenance = EnumField(values=('Yes', 'No'),null=True,blank=False) 
+#    maintenance = EnumField(values=('Yes', 'No'),null=True,blank=False) 
+    maintenance = models.CharField(verbose_name='Maintenance',max_length='3',null=False,blank=False) 
 #    maintenance = models.PositiveSmallIntegerField(null=True,blank=True)
     action = models.CharField(max_length=24,unique=False)
     class Meta:
         db_table = 'EquipmentLog'
         verbose_name = 'Equipment Log'
         verbose_name_plural = 'Equipment Logs'
+
+# Model Forms
+class WaterLogForm(ModelForm):
+    class Meta:
+        model = WaterLog
+        exclude = ('aquariumID')
--- aquarilog/views.py
+++ aquarilog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from aquarilog.models import Aquarium, WaterLog
+from django.db.models import Q, Max
+from aquarilog.models import Aquarium, Equipment, EquipmentLog, WaterLog, WaterLogForm
+
 
 def index(request):
     aquarium_list = Aquarium.objects.all().order_by('-activeSince')
@@ -13,43 +15,38 @@ def index(request):
 
 def aquarium_details(request, aquarium_id, error_message=None):
     aquarium = get_object_or_404(Aquarium, pk=aquarium_id)
+    
+    equipment = Equipment.objects.raw(
+        "SELECT Equipment.equipmentID, maintenance, action, \
+        MAX(logDate) AS lastMaint, \
+        DATEDIFF(MAX(logDate), NOW()) AS nextMaint \
+        FROM Equipment \
+        LEFT OUTER JOIN EquipmentLog ON EquipmentLog.equipmentID = Equipment.equipmentID \
+        WHERE Equipment.aquariumID = %s \
+        GROUP BY Equipment.equipmentID",
+        [aquarium_id]
+    )
+
+    # Process Water Log Entries
+    if request.method == 'POST':
+        water_log = WaterLog(aquariumID = aquarium)
+        water_log_form = WaterLogForm(request.POST, instance=water_log)
+        if water_log_form.is_valid():
+            water_log_form.save()
+            return HttpResponseRedirect(reverse('aquarilog.views.aquarium_details', args=(aquarium.aquariumID,)))
+    else:
+        water_log_form = WaterLogForm()
+        
     latest_water_logs = \
         WaterLog.objects.filter(aquariumID = aquarium_id) \
             .order_by('testedOn')[:10]
+            
     return render(request,
         'aquarilog/aquarium.html',
         {'aquarium': aquarium,
+         'equipment' : equipment,
+         'equipment_logs' : equipment_logs,
          'latest_water_logs': latest_water_logs,
-         'error_message': error_message}
+         'error_message': error_message,
+         'water_log_form': water_log_form,}
     )
-    
-def add_water_sample(request, aquarium_id):
-    aquarium = get_object_or_404(Aquarium, pk=aquarium_id)
-    log = WaterLog(aquariumID = aquarium, testedOn = timezone.now())
-    try:
-        if request.POST['temperature']:
-            if float(request.POST['temperature']) < 0:
-                raise Exception('Temperature Cannot Be 0')
-            log.temperature = request.POST['temperature']
-    except (Exception as e, WaterLog.DoesNotExist):
-        return aquarium_details(request, aquarium_id, error_message=e.value)
-
-    return HttpResponseRedirect(reverse('aquarilog.views.aquarium', args=(aquarium.aquariumID,)))
-'''
-    if request.POST['ammonia']:
-        log.ammonia = request.POST['ammonia']
-    if request.POST['nitrites']:
-        log.nitrites = request.POST['nitrite']
-    if request.POST['nitrates']:
-        log.nitrates = request.POST['nitrate']
-    if request.POST['pH']:
-        log.pH = request.POST['pH']
-    if request.POST['KH']:
-        log.KH = request.POST['KH']
-    if request.POST['ammountExchanged']:
-        log.ammountExchanged = request.POST['ammountExchanged']
-    if request.POST['comments']:
-        log.comments = request.POST['comments']
-    log.save
-'''
-    
