e115c16813dead86bf115c1239fe535d4c721dfb
Tim Soderstrom <tim@moocowproductions.org>
2012-10-28 18:49:19 -0500

    Added functionality to add water log entries

--- aquarilog/urls.py
+++ aquarilog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('aquarilog.views',
     url(r'^$', 'index'),
     url(r'^aquarium/(?P<aquarium_id>\d+)/$', 'aquarium_details'),
-#    url(r'^aquarium/(?P<aquarium_id>\d+)/add_water_sample$', 'add_water_sample'),
+    url(r'^aquarium/(?P<aquarium_id>\d+)/waterlog/(?P<waterlog_id>\d+)/$', 'waterlog_entry'),
 )
+
--- aquarilog/views.py
+++ aquarilog/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from django.db.models import Q, Max
+from django.core.exceptions import ObjectDoesNotExist
+
 from aquarilog.models import Aquarium, Equipment, EquipmentLog, WaterLog, WaterLogForm
 
 
 def index(request):
-    aquarium_list = Aquarium.objects.all().order_by('-activeSince')
+    aquarium_list = Aquarium.objects.all().order_by('-activeSince')    
     return render(request,
         'aquarilog/index.html',
         {'aquarium_list': aquarium_list}
     )
-
-def aquarium_details(request, aquarium_id, error_message=None):
+    
+def aquarium_details(request, aquarium_id):
     aquarium = get_object_or_404(Aquarium, pk=aquarium_id)
     
     equipment = Equipment.objects.raw(
         "SELECT Equipment.equipmentID, maintenance, action, \
         MAX(logDate) AS lastMaint, \
-        DATEDIFF(MAX(logDate), NOW()) AS nextMaint \
+        DATEDIFF(NOW(), MAX(logDate)) AS daysSinceMaint \
         FROM Equipment \
         LEFT OUTER JOIN EquipmentLog ON EquipmentLog.equipmentID = Equipment.equipmentID \
         WHERE Equipment.aquariumID = %s \
@@ -27,16 +28,6 @@ def aquarium_details(request, aquarium_id, error_message=None):
         [aquarium_id]
     )
 
-    # Process Water Log Entries
-    if request.method == 'POST':
-        water_log = WaterLog(aquariumID = aquarium)
-        water_log_form = WaterLogForm(request.POST, instance=water_log)
-        if water_log_form.is_valid():
-            water_log_form.save()
-            return HttpResponseRedirect(reverse('aquarilog.views.aquarium_details', args=(aquarium.aquariumID,)))
-    else:
-        water_log_form = WaterLogForm()
-        
     latest_water_logs = \
         WaterLog.objects.filter(aquariumID = aquarium_id) \
             .order_by('testedOn')[:10]
@@ -45,7 +36,29 @@ def aquarium_details(request, aquarium_id, error_message=None):
         'aquarilog/aquarium.html',
         {'aquarium': aquarium,
          'equipment' : equipment,
-         'latest_water_logs': latest_water_logs,
-         'error_message': error_message,
-         'water_log_form': water_log_form,}
+         'latest_water_logs': latest_water_logs,}
     )
+
+def waterlog_entry(request, aquarium_id, waterlog_id):
+    try:
+        waterlog = WaterLog.objects.get(pk = waterlog_id)
+    except ObjectDoesNotExist:
+        aquarium = get_object_or_404(Aquarium, pk=aquarium_id)
+        waterlog = WaterLog(aquariumID = aquarium, waterLogID = 0)
+
+    # Process Water Log Entries
+    if request.method == 'POST':
+        waterlog_form = WaterLogForm(request.POST, instance=waterlog)
+        if waterlog_form.is_valid():
+            waterlog_form.save()
+            return HttpResponseRedirect(reverse('aquarilog.views.aquarium_details', args=(aquarium_id,)))
+    else:
+        waterlog_form = WaterLogForm(instance=waterlog)
+#    else:
+#        waterlog_form = WaterLogForm()
+    return render(request,
+        'aquarilog/waterlog.html',
+        {'aquarium_id' : aquarium_id,
+         'waterlog' : waterlog,
+         'waterlog_form': waterlog_form,}
+    )   
