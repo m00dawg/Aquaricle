1575612e02adff162f7571b9c0c176292052af33
Tim Soderstrom <tim@moocowproductions.org>
2012-10-30 08:29:00 -0500

    Moved away from an ENUM tyep in Django (for now)

--- aquarilog/models.py
+++ aquarilog/models.py
@@ -6,8 +6,9 @@ from aquaridawg.fields import EnumField
 
 class WaterProfile(models.Model):
     waterProfileID = models.AutoField(primary_key=True)
-    waterType = EnumField(values=('Freshwater', 'Saltwater', 'Other'),null=True,blank=True)
+#    waterType = EnumField(values=('Freshwater', 'Saltwater', 'Other'),null=True,blank=True)
 #    waterType = models.PositiveSmallIntegerField(null=True,blank=True)
+    waterType = models.CharField(verbose_name='Water Type',max_length='10',null=False,blank=False) 
     name = models.CharField(max_length=24,unique=True)
     temperature = models.DecimalField(verbose_name='Temperature (C)',max_digits=4,decimal_places=2,null=True,blank=True)
     pH = models.DecimalField(max_digits=3,decimal_places=1,null=True,blank=True)
