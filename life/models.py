4cc358ea0c5807a8e411356e7774af04255bc88f
Tim Soderstrom <tim@moocowproductions.org>
2013-01-06 22:45:04 -0600

    Expanded life model, added email reminders to TODO

--- life/models.py
+++ life/models.py
@@ -3,9 +3,12 @@ from django.utils import timezone
 
 from aquariums.models import Aquarium
 
-class Life(models.Model):
-    lifeID = models.AutoField(primary_key=True)
+class AquariumLife(models.Model):
+    aquaLifeID = models.AutoField(primary_key=True)
+    lifeID = models.ForeignKey(Life,verbose_name='Species',db_column='lifeID')
     aquariumID = models.ForeignKey(Aquarium,verbose_name='Aquarium',db_column='aquariumID')
+
+
     name = models.CharField(max_length=64,blank=True,null=True)
     dateAdded = models.DateTimeField(
         verbose_name='Date Added',
@@ -15,5 +18,10 @@ class Life(models.Model):
     def __unicode__(self):
         return self.name
     class Meta:
-        db_table = 'Life'
+        db_table = 'AquariumLife'
         unique_together = ('aquariumID', 'name')
+        
+class Life(models.Model):
+    lifeID = models.AutoField(primary_key=True)
+    commonName = models.CharField(max_length=64,blank=True,null=True)
+    kingdom = models.CharField(max_length=64,blank=True,null=True)
