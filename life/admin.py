660a068f0b7edd86c80ead4dde367415805adb32
Tim Soderstrom <tim@moocowproductions.org>
2013-05-26 17:20:07 -0500

    LifeLog is now part of admin interface

--- life/admin.py
+++ life/admin.py
@@ -1,6 +1,8 @@
 from life.models import Life
 from life.models import AquariumLife
+from life.models import LifeLog
 from django.contrib import admin
 
 admin.site.register(Life)
 admin.site.register(AquariumLife)
+admin.site.register(LifeLog)
--- life/views.py
+++ life/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-#from life.models import AquariumLife
+from life.models import AquariumLife
+from life.models import LifeLog
 
 #def life(request):
 #    profiles = WaterProfile.objects.all()
