dfabb2f70051c5b1cb0f0dafeefca6885fe6ec0b
Tim Soderstrom <tim@moocowproductions.org>
2013-06-09 18:00:43 -0500

    Fisxed equipment log entry issue

--- equipment/views.py
+++ equipment/views.py
@@ -19,13 +19,13 @@ def equipment_details(request, equipment_id):
     equipment = get_object_or_404(Equipment, pk=equipment_id)
     logs = EquipmentLog.objects.filter(equipmentID = equipment_id)
     log_entry = EquipmentLog(equipmentID = equipment)        
-    log_form = EquipmentLogForm(instance=log_entry)
+    equipment_log_form = EquipmentLogForm(instance=log_entry)
 
     # Process Equipment Log Entries
     if request.method == 'POST':
-        log_form = EquipmentLogForm(request.POST, instance=log_entry)
+        equipment_log_form = EquipmentLogForm(request.POST, instance=log_entry)
         if log_form.is_valid():
-            new_log = log_form.save()
+            new_log = equipment_log_form.save()
             return HttpResponseRedirect(
                 reverse('equipment.views.equipment_details', 
                 args=(equipment_id,)))
@@ -34,5 +34,5 @@ def equipment_details(request, equipment_id):
         'equipment_details.html',
         {'equipment' : equipment,
          'logs' : logs,
-         'log_form' : log_form}
+         'equipment_log_form' : equipment_log_form}
     )
