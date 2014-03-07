e7a44cc57529a39c6b98f598c1899809a9f1d2b2
Tim Soderstrom <tim@moocowproductions.org>
2014-03-07 12:00:44 -0600

    Put new graphs on web interface, fixed filenames

--- rrd/graph.sh
+++ rrd/graph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ID=1
 cd /home/tim/git/aquaricle/rrd
-rrdtool graph ../static/graphs/$ID-temp-thumb.png \
+rrdtool graph ../static/graphs/$ID-temps-thumb.png \
 -t "Aquarium Temperature" \
 -w 480 -h 120 \
 -E \
@@ -12,7 +12,7 @@ LINE1:temperature#FF0000:"Temperature" \
 GPRINT:lastTemperature:"Current\: %2.2lfC" \
 GPRINT:avgTemperature:" Average\: %2.2lfC\l"
 
-rrdtool graph ../static/graphs/$ID-temp-full.png \
+rrdtool graph ../static/graphs/$ID-temps-full.png \
 -t "Aquarium Temperature" \
 -w 1280 -h 640 \
 -E \
