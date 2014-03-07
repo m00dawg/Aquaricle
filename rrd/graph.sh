d9c87cd0b71dab4b6c38ac7070f388d09f3e18ef
Tim Soderstrom <tim@moocowproductions.org>
2014-03-07 11:57:03 -0600

    Split relays and temps back into 2 graphs

--- rrd/graph.sh
+++ rrd/graph.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 ID=1
 cd /home/tim/git/aquaricle/rrd
-rrdtool graph ../static/graphs/$ID-thumb.png \
+rrdtool graph ../static/graphs/$ID-temp-thumb.png \
 -t "Aquarium Temperature" \
 -w 480 -h 120 \
 -E \
 DEF:temperature=$ID.rrd:temperature:AVERAGE \
-DEF:heater=$ID.rrd:heater:AVERAGE \
-DEF:light=$ID.rrd:light:AVERAGE \
-CDEF:weightedHeater=heater,20,* \
-CDEF:weightedLight=light,20,* \
 VDEF:lastTemperature=temperature,LAST \
 VDEF:avgTemperature=temperature,AVERAGE \
-LINE3:temperature#FF0000:"Temperature" \
+LINE1:temperature#FF0000:"Temperature" \
 GPRINT:lastTemperature:"Current\: %2.2lfC" \
-GPRINT:avgTemperature:" Average\: %2.2lfC\l" \
-AREA:heater#FFFF00:"Heater\l":STACK \
-AREA:light#AAAAFF:"Light\l":STACK
+GPRINT:avgTemperature:" Average\: %2.2lfC\l"
 
-rrdtool graph ../static/graphs/$ID-full.png \
+rrdtool graph ../static/graphs/$ID-temp-full.png \
 -t "Aquarium Temperature" \
 -w 1280 -h 640 \
 -E \
 DEF:temperature=$ID.rrd:temperature:AVERAGE \
-DEF:heater=$ID.rrd:heater:AVERAGE \
-DEF:light=$ID.rrd:light:AVERAGE \
-CDEF:weightedHeater=heater,20,* \
-CDEF:weightedLight=light,20,* \
 VDEF:lastTemperature=temperature,LAST \
 VDEF:avgTemperature=temperature,AVERAGE \
-LINE3:temperature#FF0000:"Temperature" \
+LINE1:temperature#FF0000:"Temperature" \
 GPRINT:lastTemperature:"Current\: %2.2lfC" \
-GPRINT:avgTemperature:" Average\: %2.2lfC\l" \
-AREA:heater#FFFF00:"Heater\l":STACK \
-AREA:light#AAAAFF:"Light\l":STACK
+GPRINT:avgTemperature:" Average\: %2.2lfC\l"
 
+rrdtool graph ../static/graphs/$ID-relays-thumb.png \
+-t "Aquarium Relays" \
+-w 480 -h 120 \
+-E \
+DEF:heater=$ID.rrd:heater:AVERAGE \
+DEF:light=$ID.rrd:light:AVERAGE \
+AREA:light#AAAAFF:"Light\l":STACK \
+AREA:heater#FFFF00:"Heater\l":STACK 
+
+rrdtool graph ../static/graphs/$ID-temp-full.png \
+-t "Aquarium Temperature" \
+-w 1280 -h 640 \
+-E \
+DEF:heater=$ID.rrd:heater:AVERAGE \
+DEF:light=$ID.rrd:light:AVERAGE \
+AREA:light#AAAAFF:"Light\l":STACK \
+AREA:heater#FFFF00:"Heater\l":STACK 
