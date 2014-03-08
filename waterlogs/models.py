a66c105b66e0829c3dcf33e4f8ce57a9923e1cd7
Tim Soderstrom <tim@moocowproductions.org>
2014-03-08 17:16:42 -0600

    New test schema design, phosphates
    Added a new schema design to better approach how to handle log entries
    across different sections (e.g. EquipmentLog, WaterLogs, etc.).
    
    Also added phosphates column to water logs.

--- waterlogs/models.py
+++ waterlogs/models.py
@@ -33,6 +33,7 @@ class WaterLog(models.Model):
     nitrates = models.DecimalField(max_digits=5,decimal_places=2,null=True,blank=True)
     pH = models.DecimalField(verbose_name='pH',max_digits=3,decimal_places=1,null=True,blank=True)
     KH = models.PositiveSmallIntegerField(null=True,blank=True)
+    phosphates = models.DecimalField(max_digits=5,decimal_places=2,null=True,blank=True)
     amountExchanged = models.PositiveSmallIntegerField(verbose_name='Liters Exchanged',null=True,blank=True)
     comments = models.TextField(blank=True)
     def __unicode__(self):
@@ -48,5 +49,5 @@ class WaterLog(models.Model):
 class WaterLogForm(ModelForm):
     class Meta:
         model = WaterLog
-        fields = ('testedOn', 'waterAdditiveID', 'additiveAmount', 'amountExchanged', 'ammonia', 'nitrites', 'nitrates', 'pH', 'KH', 'comments')
+        fields = ('testedOn', 'waterAdditiveID', 'additiveAmount', 'amountExchanged', 'ammonia', 'nitrites', 'nitrates', 'pH', 'KH', 'phosphates', 'comments')
         exclude = ('aquariumID',)
