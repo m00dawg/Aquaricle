e45041a41e046544560e38fa3d6bd3e5e9c509da
Tim Soderstrom <tim@moocowproductions.org>
2014-05-16 09:52:35 -0500

    Added very basic user functions

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
-from django.contrib.auth.models import AbstractBaseUser
+#from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from django.contrib.auth import hashers 
 from users.managers import UserManager
 
 
-class User(AbstractBaseUser):
+class User(models.Model):
     userID = models.AutoField(primary_key=True)
     username = models.CharField(verbose_name='Username',max_length='32',null=False,blank=False,unique=True)
+    password = models.CharField(verbose_name='Password',max_length='128',null=False,blank=False)
     email = models.CharField(verbose_name='E-Mail',max_length='255',null=False,blank=False)
+    
     '''
     Since we're extending the AbstractBaseUser, a password field is already defined
     password = ...
@@ -24,7 +27,10 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
     def is_active(self):
-        return true
+        return True
+    def set_password(self, password):
+        self.password = hashers.make_password(password)
+        
     class Meta:
             db_table = 'Users'
     objects = UserManager();
