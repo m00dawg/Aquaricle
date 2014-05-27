12693ed4249e8768e697ccbe394c2e3ead5fc266
Tim Soderstrom <tim@moocowproductions.org>
2014-05-26 23:24:06 -0500

    Added stub for waterlog entries for the main add log form

--- app/views/modifylog.blade.php
+++ app/views/modifylog.blade.php
@@ -17,15 +17,36 @@
 	@if (isset($log))
 		{{ Form::model($log, array('route' => array("aquariums.logs.update", $aquariumID, $log->aquariumLogID), 'method' => 'PUT')) }}		
 		
-		
-		
-		
 	@else
 		{{ Form::open(array('url' => "aquariums/$aquariumID/logs")) }}
 	@endif
 	
 		{{ Form::label('logDate', 'Date') }}: {{ Form::text('logDate') }}<br />
 		{{ Form::label('comments', 'Comments') }}: {{ Form::textarea('comments') }}<br />
+		
+		<h4>Water Logs</h4>
+		
+		<table>
+			<tr>
+				<th>Temperature</th>
+				<th>Ammonia</th>
+				<th>Nitrites</th>
+				<th>Nitrates</th>
+				<th>Phosphates</th>
+				<th>pH</th>
+				<th>KH</th>
+			</tr>
+			<tr>
+				<td>{{ Form::text('temperature', null, array('size' => '8')) }}</td>
+				<td>{{ Form::text('ammonia', null, array('size' => '8')) }}</td>
+				<td>{{ Form::text('nitrites', null, array('size' => '8')) }}</td>
+				<td>{{ Form::text('nitrates', null, array('size' => '8')) }}</td>
+				<td>{{ Form::text('phosphates', null, array('size' => '8')) }}</td>
+				<td>{{ Form::text('pH', null, array('size' => '8')) }}</td>
+				<td>{{ Form::text('kH', null, array('size' => '8')) }}</td>
+			</tr>
+			
+		</table>
 		
 		@if (isset($log))
 			{{ Form::submit('Update') }}
