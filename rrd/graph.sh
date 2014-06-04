45dc8a80aa010895c19f030e2f78a52d1d9c77d7
Tim Soderstrom <tim@moocowproductions.org>
2014-06-03 23:05:52 -0500

    Update graph.sh for Laravel path

--- rrd/graph.sh
+++ rrd/graph.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 ID=1
 cd /home/tim/git/aquaricle/rrd
-rrdtool graph ../static/graphs/$ID-temps-thumb.png \
+rrdtool graph ../public/static/graphs/$ID-temps-thumb.png \
 -t "Aquarium Temperature" \
 -w 480 -h 120 \
 -E \
 DEF:temperature=$ID.rrd:temperature:AVERAGE \
 VDEF:lastTemperature=temperature,LAST \
+VDEF:minTemperature=temperature,MINIMUM \
+VDEF:maxTemperature=temperature,MAXIMUM \
 VDEF:avgTemperature=temperature,AVERAGE \
 LINE1:temperature#FF0000:"Temperature" \
 GPRINT:lastTemperature:"Current\: %2.2lfC" \
+GPRINT:minTemperature:"Min\: %2.2lfC" \
+GPRINT:maxTemperature:"Max\: %2.2lfC" \
 GPRINT:avgTemperature:" Average\: %2.2lfC\l"
 
-rrdtool graph ../static/graphs/$ID-temps-full.png \
+rrdtool graph ../public/static/graphs/$ID-temps-full.png \
 -t "Aquarium Temperature" \
 -w 1280 -h 640 \
 -E \
 DEF:temperature=$ID.rrd:temperature:AVERAGE \
 VDEF:lastTemperature=temperature,LAST \
+VDEF:minTemperature=temperature,MINIMUM \
+VDEF:maxTemperature=temperature,MAXIMUM \
 VDEF:avgTemperature=temperature,AVERAGE \
 LINE1:temperature#FF0000:"Temperature" \
 GPRINT:lastTemperature:"Current\: %2.2lfC" \
+GPRINT:minTemperature:"Min\: %2.2lfC" \
+GPRINT:maxTemperature:"Max\: %2.2lfC" \
 GPRINT:avgTemperature:" Average\: %2.2lfC\l"
 
 rrdtool graph ../static/graphs/$ID-relays-thumb.png \
