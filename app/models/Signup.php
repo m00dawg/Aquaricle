b94d62ce2f042c7cd17d9b49efe89889584c3423
Tim Soderstrom <tim@moocowproductions.org>
2014-07-13 20:32:57 -0500

    Signup model wasn't added in

---
+++ app/models/Signup.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Signup extends BaseModel
+{
+	protected $table = 'Signups';
+	protected $primaryKey = 'username';
+	protected $guarded = array('username, email, token');
+	public $timestamps = true;
+}
