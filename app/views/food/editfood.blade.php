6a83438c5832abb6ead80d1ff38388d9984fa6b2
Tim Soderstrom <tim@moocowproductions.org>
2014-07-19 23:47:02 -0500

    Forgot to add edit food blade

---
+++ app/views/food/editfood.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+@section('content')
+
+<h2>Food</h2>
+
+@foreach ($errors->all() as $message)
+	<h4>{{ $message }}</h4>
+@endforeach
+
+<div class="formBox">
+	@if (isset($food))
+		{{ Form::model($food, 
+			array('route' => array("food.edit", $food->foodID), 'method' => 'post')) }}		
+	@else
+		{{ Form::open(array('url' => "food/edit")) }}
+	@endif
+
+	<table>
+		<tr>
+			<th>Name</th>
+			<td>{{ Form::text('name', $food->name) }}</td>
+		</tr>
+		<tr>	
+			<th>Description</th>
+			<td>{{ Form::textarea('description', $food->description) }}</td>
+		</tr>
+	</table>
+	<br />
+	@if (isset($food))
+		{{ Form::submit('Update') }}
+		<input type="submit" name="delete" value="Delete">
+	@else
+		{{ Form::submit('Add') }}
+	@endif	
+
+	{{ Form::close() }}
+</div>
+
+
+@stop
