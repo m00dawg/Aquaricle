6cb284287049d07e3c3706d2b58453832faa16f2
Tim Soderstrom <tim@moocowproductions.org>
2014-07-27 09:05:39 -0500

    Ooops

--- app/views/aquariums/aquarium.blade.php
+++ app/views/aquariums/aquarium.blade.php
@@ -68,7 +68,7 @@
 	@endif
 </ul>
 
-($aquarium->sparkID && $aquarium->sparkToken)
+@if ($aquarium->sparkID && $aquarium->sparkToken)
 	<h3>Graphs</h3>
 	<div id="graph">
 	<a href="/static/graphs/{{ $aquarium->aquariumID }}-temps-full.png">
