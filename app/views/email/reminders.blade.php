34c2731e232b29c83f5d770b38c1348fc26f40d6
Tim Soderstrom <tim@moocowproductions.org>
2014-07-30 13:34:37 -0500

    Added unsub instructions for e-mails

--- app/views/email/reminders.blade.php
+++ app/views/email/reminders.blade.php
@@ -3,3 +3,7 @@ Hi {{ $username }}!
 This is a reminder of some tasks that will need to be taken care of for your aquarium(s):
 
 {{ $body }}
+
+
+---
+If you no longer wish to receive these e-mails, simply login and go to User Preferences (http://aquaridev/user/editprofile) and set "Uncheck E-mail Reminders" to No.
