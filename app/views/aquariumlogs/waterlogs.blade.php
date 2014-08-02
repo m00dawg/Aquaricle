7768590205290dd537b776aa1c7f6966aa99ce04
Tim Soderstrom <tim@moocowproductions.org>
2014-08-02 14:13:33 -0500

    Re-arrange JS for if checks

--- app/views/aquariumlogs/waterlogs.blade.php
+++ app/views/aquariumlogs/waterlogs.blade.php
@@ -85,65 +85,6 @@
 	<script>
 		(function()
 		{
-			var waterCycleGraph = document.getElementById('waterCycleGraph').getContext('2d');
-			var waterCycleData = 
-			{
-				labels: {{ json_encode($cycleLogDateList) }},
-				datasets: [
-					{
-						label: "Nitrates",
-						data: {{ json_encode($nitrateList) }},
-						fillColor: "rgba(250, 100, 00, 0.75)",
-						strokeColor: "#ff1100",
-						pointColor: "#ffff00",
-					},
-					{
-						label: "Nitrites",
-						data: {{ json_encode($nitriteList) }},
-						fillColor: "rgba(150, 0, 150, 0.5)",
-						strokeColor: "#cc00cc",
-						pointColor: "#ff00ff",
-					},
-					{
-						label: "Ammonia",
-						data: {{ json_encode($ammoniaList) }},
-						fillColor: "rgba(0, 50, 0, 0.5)",
-						strokeColor: "#00bb00",
-						pointColor: "#00ff00",
-					},
-				]
-			};
-
-			var phosphatesGraph = document.getElementById('waterPhosphatesGraph').getContext('2d');
-			var phosphatesData = 
-			{
-				labels: {{ json_encode($phoshateLogDateList) }},
-				datasets: [
-					{
-						label: "Phosphates",
-						data: {{ json_encode($phoshateDataList) }},
-						fillColor: "rgba(250, 250, 00, 0.75)",
-						strokeColor: "#ffcc00",
-						pointColor: "#ffff00",
-					},
-				]
-			};
-	
-			var waterChangeGraph = document.getElementById('waterChangeGraph').getContext('2d');
-			var waterChangeData = 
-			{
-				labels: {{ json_encode($waterChangeDateList) }},
-				datasets: [
-					{
-						label: "Water Changes",
-						data: {{ json_encode($waterChangeDataList) }},
-						fillColor: "rgba(0, 0, 250, 0.25)",
-						strokeColor: "#0000bb",
-						pointColor: "#4444ff",
-					},
-				]
-			};
-	
 			Chart.defaults.global.responsive = true;
 			Chart.defaults.global.maintainAspectRatio = false;
 			Chart.defaults.global.tooltipTitleFontSize = 12;
@@ -153,17 +94,78 @@
 			Chart.defaults.global.scaleGridLineColor = "#ccccff";
 			
 			@if (count($cycleLogDateList) > 1)
+				var waterCycleGraph = document.getElementById('waterCycleGraph').getContext('2d');
+				var waterCycleData = 
+				{
+					labels: {{ json_encode($cycleLogDateList) }},
+					datasets: [
+						{
+							label: "Nitrates",
+							data: {{ json_encode($nitrateList) }},
+							fillColor: "rgba(250, 100, 00, 0.75)",
+							strokeColor: "#ff1100",
+							pointColor: "#ffff00",
+						},
+						{
+							label: "Nitrites",
+							data: {{ json_encode($nitriteList) }},
+							fillColor: "rgba(150, 0, 150, 0.5)",
+							strokeColor: "#cc00cc",
+							pointColor: "#ff00ff",
+						},
+						{
+							label: "Ammonia",
+							data: {{ json_encode($ammoniaList) }},
+							fillColor: "rgba(0, 50, 0, 0.5)",
+							strokeColor: "#00bb00",
+							pointColor: "#00ff00",
+						},
+					]
+				};
+				
 				basicTestsChart = new Chart(waterCycleGraph).Line(waterCycleData, { 
 					bezierCurve: false
 				}); 
+
 			@endif
+
 			@if (count($phoshateDataList) > 1)
+				var phosphatesGraph =
+					 document.getElementById('waterPhosphatesGraph').getContext('2d');
+				var phosphatesData = 
+				{
+					labels: {{ json_encode($phoshateLogDateList) }},
+					datasets: [
+						{
+							label: "Phosphates",
+							data: {{ json_encode($phoshateDataList) }},
+							fillColor: "rgba(250, 250, 00, 0.75)",
+							strokeColor: "#ffcc00",
+							pointColor: "#ffff00",
+						},
+					]
+				};
 				phosphatesChart = new Chart(phosphatesGraph).Line(phosphatesData, { 
 					bezierCurve: false
 				}); 
 			@endif
-			
+	
 			@if (count($waterChangeDataList) > 1)
+				var waterChangeGraph = document.getElementById('waterChangeGraph').getContext('2d');
+				var waterChangeData = 
+				{
+					labels: {{ json_encode($waterChangeDateList) }},
+					datasets: [
+						{
+							label: "Water Changes",
+							data: {{ json_encode($waterChangeDataList) }},
+							fillColor: "rgba(0, 0, 250, 0.25)",
+							strokeColor: "#0000bb",
+							pointColor: "#4444ff",
+						},
+					]
+				};
+			
 				waterChangeChart = new Chart(waterChangeGraph).Line(waterChangeData, { 
 					bezierCurve: false
 				}); 
