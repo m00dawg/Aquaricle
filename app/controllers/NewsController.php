fb58a43a81f2fb1f9bf6dff73d365e936211f4b7
Tim Soderstrom <tim@moocowproductions.org>
2014-08-15 12:42:53 -0500

    Fixed the ordering of the news

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -4,7 +4,8 @@ class NewsController extends \BaseController
 {
 	public function index()
 	{
-		$news = News::paginate(10);
+		$news = News::orderBy('createdAt', 'desc')
+			->paginate(10);
 			
 		return View::make('index')
 			->with('news', $news);
